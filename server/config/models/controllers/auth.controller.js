a70c39edace52c024ec2461e63a195463fa8979e
Hemlata0708-h <hemlatasanadhya2@gmail.com>
2026-04-03 21:08:42 +0530

    Change cookie option from 'http' to 'httpOnly'

--- server/config/models/controllers/auth.controller.js
+++ server/config/models/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const googleAuth = async (req, res) => {
     }
     let token = await getToken(user._id);
     res.cookie("token", token, {
-      http: true,
+      httpOnly: true,
       secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
